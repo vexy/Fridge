ad11a3ef3b23c50f44a06e46da4a62fe7fe369c4
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2016-12-23 19:40:21 +0100

    FridgeItem refactoring
    - refactored initializers & setter methods
    - added more tests to support new logic (targets : initializers & assignment methods)
      - tests now cover various init() methods to make sure they work as expected
      - tests now cover FridgeItem.url assignment method to make sure it works as expected

--- FridgeTests/FridgeItemTests.swift
+++ FridgeTests/FridgeItemTests.swift
@@ -11,30 +11,95 @@ import Darwin
 
 class FridgeItemTests: XCTestCase {
     
-    func testEmptyStringThrows() {
-        XCTAssertThrowsError(_  = try FridgeItem(withString: ""))
+    //declaration of mock objects :
+    //Strings
+    let emptyString               = ""
+    let randomString              = "someRandomString (lsaidh 129418ycs clihrq)"
+    let otherThanHTTPSchemeString = "ftp://foo.at.bar/folder"
+    //
+    let validator                 = "https://www.google.com"
+    
+    
+    //URL's
+    let validURL =   URL(string: "http://www.apple.com")!
+    let fakeURL  =   URL(string: "someFakeURL_initializedWithString")!
+    let nillURL  =   URL(string: "")          //will make this let an optional !!!
+    
+    
+    //FRIDGE item
+    var fi = FridgeItem()
+    
+    
+    //MARK:- Tests of object initializers that accept 'String'
+    func testInitWithEmptyStringThrows() {
+        XCTAssertThrowsError(_  = try FridgeItem(withString: emptyString))
     }
     
-    func testRandomStringThrows() {
-        XCTAssertThrowsError(_ = try FridgeItem(withString: "someString"))
+    func testInitWithRandomStringThrows() {
+        XCTAssertThrowsError(_ = try FridgeItem(withString: randomString))
     }
     
-    func testFTPStringThrows() {
-        XCTAssertThrowsError(_ = try FridgeItem(withString: "ftp://foo.bar"))
+    func testInitWithFTPStringThrows() {
+        XCTAssertThrowsError(_ = try FridgeItem(withString: otherThanHTTPSchemeString))
     }
     
-    func testValidStringPasses() {
+    func testInitWithValidStringDontThrow() {
         do {
-            _ = try FridgeItem(withString: "https://www.google.com")
+            _ = try FridgeItem(withString: validator)
         } catch {
             XCTFail("Regular URL item throws error !")
         }
     }
     
-    func testFakeURLObject() {
-        let fake = URL(string: "someString")
-        let t = FridgeItem(withURL: fake)
+    
+    //MARK:- Test of object initializers that accept 'URL'
+    func testInitWithValidURLPasses() {
+        fi = try! FridgeItem(withURL: validURL)
         
-        XCTAssert(t.url.absoluteString == fake!.absoluteString)
+        XCTAssert(fi.url.absoluteString == validURL.absoluteString, "Valid URL object isn't assigned to struct property !")
     }
+    
+    func testInitWithFakeURLThrows() {
+        XCTAssertThrowsError( _ = try FridgeItem(withURL: fakeURL) )
+    }
+    
+    func testInitWithNillURLThrows() {
+        XCTAssertThrowsError( _ = try FridgeItem(withURL: nillURL))
+    }
+    
+    func testInitWithFakeURLResetsToDefaultValue() {
+        do {
+            fi = try FridgeItem(withURL: fakeURL)
+            
+        } catch FridgeError.invalidScheme {
+            XCTAssertFalse(fi.url.absoluteString == validator, "Initialization with fake URL doesn't reset value !")
+        } catch {
+            XCTFail("Something is thrown!")
+        }
+    }
+    
+    func testInitWithNilURLResetsToDefaultValue() {
+        do {
+            fi = try FridgeItem(withURL: nillURL)
+        } catch FridgeError.generalError {
+            XCTAssertFalse(fi.url.absoluteString == validator, "Initialization with nil URL doesn't reset value")
+        } catch {
+            XCTFail("Something is thrown!")
+        }
+    }
+    
+    
+    //MARK:- URL Assignments test
+    func testAssignInvalidURLSchemeResetsToDefaultsValue() {
+        fi.url = URL(string: otherThanHTTPSchemeString)!
+        
+        XCTAssertFalse(fi.url.absoluteString == validator, "Assigning URL with invalid scheme does not defaults property !")
+    }
+    
+    func testAssignFakeURLDefaultsResetsToDefaultValue() {
+        fi.url = fakeURL
+        
+        XCTAssertFalse(fi.url.absoluteString == fakeURL.absoluteString, "Assigning fake URL doesn't default property !")
+    }
+    
 }
